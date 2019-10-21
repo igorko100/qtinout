54a309368cf044dcf292d371fa196e29546489c6
Igor Kozlov <igorkozlov862@gmail.com>
2019-10-21 23:16:12 +0300

    hex

--- showlasteventdialog.cpp
+++ showlasteventdialog.cpp
@@ -9,20 +9,23 @@ ShowLastEventDialog::ShowLastEventDialog(QString &pib, QString &status, QString 
 {
     ui->setupUi(this);
 
-    ui->labelForPixmap->setPixmap(QPixmap("at_work.png"));
     this->setStyleSheet("background-color:white;");
 
 
     ui->PibLabel->setText(pib);
-    ui->timestampLabel->setText(timestamp);
+//    ui->timestampLabel->setText(timestamp);
 
     QString date = timestamp.mid(0, 10);
     QString time = timestamp.mid(10, -1);
 
-    if(status == "in")
+    if(status == "in") {
         ui->statusLabel->setText(QString("is at work since: ") + time + " Date: " + date);
-    else
+        ui->labelForPixmap->setPixmap(QPixmap("at_work.png"));
+    }
+    else {
         ui->statusLabel->setText(QString("went out at:") + time + " Date: " + date);
+        ui->labelForPixmap->setPixmap(QPixmap("at_home.png"));
+    }
 
     QTimer::singleShot(5000, this, SLOT(close()));
 
